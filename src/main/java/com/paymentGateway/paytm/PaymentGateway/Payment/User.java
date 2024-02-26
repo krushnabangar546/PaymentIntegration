f281a5d7155ae6cc8a61b83c2d84f11422b108bb
Krushna Bangar <71035359+krushnabangar546@users.noreply.github.com>
2024-02-26 15:05:44 +0530

    message

---
+++ src/main/java/com/paymentGateway/paytm/PaymentGateway/Payment/User.java
@@ -0,0 +1,17 @@
+package com.paymentGateway.paytm.PaymentGateway.Payment;
+
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    private int amount;
+    private int cust_Id;
+
+
+}
